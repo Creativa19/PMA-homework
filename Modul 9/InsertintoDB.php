5a98c5948a5e7f04319d7bac0ff49899f5bad05c
Creativa19 <78624601+Creativa19@users.noreply.github.com>
2021-02-12 19:25:29 +0200

    Add files via upload

---
+++ Modul 9/InsertintoDB.php
@@ -0,0 +1,47 @@
+
+
+
+<?php
+function InsertIntoDB($Type, $Date, $Location, $ClientID, $Browser, $Message) {
+	
+$servername = "bankserver";
+$username = "username";
+$password = "password";
+$dbname = "bankdatabase";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection with the DB failed: " . $conn->connect_error);
+}
+$sql = "INSERT INTO LogData (Type, Date, Location, ClientId, Browser, Message);
+VALUES ($Type, $Date, $Location, $ClientID, $Browser, $Message);
+
+}
+
+
+if (strlen($username) < 15) 
+{
+    InsertIntoDB(0, "2020-02-11", "Sofia", 123456789, "Mozzila", "Success!");
+
+}
+else
+     InsertIntoDB(1,"2020-02-11", "Sofia", 123456789, "Mozzila", "Username too long!");
+	 
+if (strlen($password) < 8)
+     InsertIntoDB(2, "20-02-11", "Sofia", 123456789, "Mozilla", "Your password is too weak!");
+	 
+else 
+     InsertIntoDB(0, "20-02-11", "Sofia", 123456789, "Mozilla", "Password Success!");
+
+	
+?>
+
+
+
+
+
+
+
+
+
